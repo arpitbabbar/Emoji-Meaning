0bdb2a52daa12d112f3281f61226bf4bc28f95a7
Arpit Babbar <arpitbabbar01@gmail.com>
2022-01-02 23:26:21 +0530

    added contacts and some styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,31 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
-    const myStyle = {
-        position : "fixed",
-        bottom: "0",
-        left: "0",
-        // backgroundColor: "red",
-        height: "10vh",
-        width: "100%",
-        textAlign: "center",
-        fontSize: "1rem",
-        fontWeight: "bold"
-        
-    }
-    return (
-        <>
-         <div className='footer' style={myStyle}>
-            Made with ❤️ by <a href="https://www.instagram.com/be.like.ab/">Arpit Babbar</a>
-            <div><a>Github</a> <a>Twitter</a> <a>Youtube</a></div>
-        </div>   
-        </>
-    )
+  const myStyle = {
+    position: "fixed",
+    bottom: "0",
+    left: "0",
+    // backgroundColor: "red",
+    height: "10vh",
+    width: "100%",
+    textAlign: "center",
+    fontSize: "1rem",
+    fontWeight: "bold",
+  };
+
+  return (
+    <>
+      <div className="footer" style={myStyle}>
+        Made with ❤️ by{" "}
+        <a href="https://www.instagram.com/be.like.ab/">Arpit Babbar</a>
+        <div style={{ fontStyle: "italic" }}>
+          <a style={{"color":"red"}} href="https://github.com/arpitbabbar">Github</a>{" "}
+          <a style={{"color":"red"}} href="https://twitter.com/ArpitBabbar">Twitter</a>{" "}
+          <a style={{"color":"red"}} href="https://www.linkedin.com/in/arpitbabbar/">Linkedin</a>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
