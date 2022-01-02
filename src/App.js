81a4770236b7bd710dc6e12aabf6d4c6192254ef
Arpit Babbar <arpitbabbar01@gmail.com>
2022-01-02 19:13:58 +0530

    added background and some common emojis

--- src/App.js
+++ src/App.js
@@ -2,8 +2,25 @@ import React,{useState} from 'react';
 import './App.css';
 var emoji = require('node-emoji');
 
-function App() {
+const emojiDict = {
+  "🐰" : "rabbit",
+  "👅" : "tongue",
+  "💍" : "ring",
+  "💧" : "droplet",
+  "🍕": "pizza",
+  "🎸": "guitar",
+  "💪" : "muscle",
+  "💻" : "computer",
+  "💼" : "briefcase",
+  "📔" : "notebook_with_decorative_cover",
+  "📕" : "closed_book",
+  "😍" : "heart_eyes",
+}
 
+var knownemoji = Object.keys(emojiDict);
+
+function App() {
+  
   const [emojii, setemoji] = useState('');
   const handleEvent = (event) => {
     const userInput = event.target.value;
@@ -17,13 +34,22 @@ function App() {
     console.log(mean);
   }
 
+  const handleClick = (emo) => {
+    setemoji(emojiDict[emo]);
+  }
+
 
 
   return (
     <div className='App'> 
     <h1>Know the Meaning of Emoji</h1>
     <input placeholder='Enter or paste your emoji here' onChange={handleEvent}></input>
-    <h3>{emojii}</h3>
+    <h2>{emojii}</h2>
+    <h3>Some Common Emojis...</h3>
+    {
+    knownemoji.map(function(ele){
+      return <span style={{"padding":"1rem", "fontSize":"2rem", "cursor":"pointer"}} onClick={()=>handleClick(ele)} key={ele}>{ele}</span>
+    })}
 
     </div>
   );
