199888962d268f6b134cc114c23e6dcc4d8d3305
Arpit Babbar <arpitbabbar01@gmail.com>
2022-01-02 18:13:25 +0530

    Know the Meaning of Emoji

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+var emoji = require('node-emoji');
 
 function App() {
+
+  const [emojii, setemoji] = useState('');
+  const handleEvent = (event) => {
+    const userInput = event.target.value;
+    const mean = emoji.which(userInput);
+    if(mean===undefined){
+      setemoji("Sorry! We don't know the meaning of this emoji!");
+    }
+    else{
+    setemoji(mean);
+    }
+    console.log(mean);
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'> 
+    <h1>Know the Meaning of Emoji</h1>
+    <input placeholder='Enter or paste your emoji here' onChange={handleEvent}></input>
+    <h3>{emojii}</h3>
+
     </div>
   );
 }
